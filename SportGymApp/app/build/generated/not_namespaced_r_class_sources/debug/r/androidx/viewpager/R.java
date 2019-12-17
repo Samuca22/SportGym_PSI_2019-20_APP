d421ac81ca43ad84df2baf4eede85481d4b20a80
Samuel Martins <2180641@my.ipleiria.pt>
2019-12-17 19:54:28 +0000

    Alguns designs feitos

--- SportGymApp/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/viewpager/R.java
+++ SportGymApp/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/viewpager/R.java
@@ -62,18 +62,18 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f060061;
-        public static final int notification_bg = 0x7f060062;
-        public static final int notification_bg_low = 0x7f060063;
-        public static final int notification_bg_low_normal = 0x7f060064;
-        public static final int notification_bg_low_pressed = 0x7f060065;
-        public static final int notification_bg_normal = 0x7f060066;
-        public static final int notification_bg_normal_pressed = 0x7f060067;
-        public static final int notification_icon_background = 0x7f060068;
-        public static final int notification_template_icon_bg = 0x7f060069;
-        public static final int notification_template_icon_low_bg = 0x7f06006a;
-        public static final int notification_tile_bg = 0x7f06006b;
-        public static final int notify_panel_notification_icon_bg = 0x7f06006c;
+        public static final int notification_action_background = 0x7f060069;
+        public static final int notification_bg = 0x7f06006a;
+        public static final int notification_bg_low = 0x7f06006b;
+        public static final int notification_bg_low_normal = 0x7f06006c;
+        public static final int notification_bg_low_pressed = 0x7f06006d;
+        public static final int notification_bg_normal = 0x7f06006e;
+        public static final int notification_bg_normal_pressed = 0x7f06006f;
+        public static final int notification_icon_background = 0x7f060070;
+        public static final int notification_template_icon_bg = 0x7f060071;
+        public static final int notification_template_icon_low_bg = 0x7f060072;
+        public static final int notification_tile_bg = 0x7f060073;
+        public static final int notify_panel_notification_icon_bg = 0x7f060074;
     }
     public static final class id {
         private id() {}
@@ -86,26 +86,26 @@ public final class R {
         public static final int async = 0x7f07003d;
         public static final int blocking = 0x7f070040;
         public static final int chronometer = 0x7f070047;
-        public static final int forever = 0x7f070057;
-        public static final int icon = 0x7f07005d;
-        public static final int icon_group = 0x7f07005e;
-        public static final int info = 0x7f070061;
-        public static final int italic = 0x7f070063;
-        public static final int line1 = 0x7f070065;
-        public static final int line3 = 0x7f070066;
-        public static final int normal = 0x7f07006e;
-        public static final int notification_background = 0x7f07006f;
-        public static final int notification_main_column = 0x7f070070;
-        public static final int notification_main_column_container = 0x7f070071;
-        public static final int right_icon = 0x7f07007c;
-        public static final int right_side = 0x7f07007d;
-        public static final int tag_transition_group = 0x7f0700a2;
-        public static final int tag_unhandled_key_event_manager = 0x7f0700a3;
-        public static final int tag_unhandled_key_listeners = 0x7f0700a4;
-        public static final int text = 0x7f0700a5;
-        public static final int text2 = 0x7f0700a6;
-        public static final int time = 0x7f0700a9;
-        public static final int title = 0x7f0700aa;
+        public static final int forever = 0x7f070059;
+        public static final int icon = 0x7f07005f;
+        public static final int icon_group = 0x7f070060;
+        public static final int info = 0x7f070063;
+        public static final int italic = 0x7f070065;
+        public static final int line1 = 0x7f070067;
+        public static final int line3 = 0x7f070068;
+        public static final int normal = 0x7f070071;
+        public static final int notification_background = 0x7f070072;
+        public static final int notification_main_column = 0x7f070073;
+        public static final int notification_main_column_container = 0x7f070074;
+        public static final int right_icon = 0x7f07007f;
+        public static final int right_side = 0x7f070080;
+        public static final int tag_transition_group = 0x7f0700a5;
+        public static final int tag_unhandled_key_event_manager = 0x7f0700a6;
+        public static final int tag_unhandled_key_listeners = 0x7f0700a7;
+        public static final int text = 0x7f0700a8;
+        public static final int text2 = 0x7f0700a9;
+        public static final int time = 0x7f0700ac;
+        public static final int title = 0x7f0700ad;
     }
     public static final class integer {
         private integer() {}
@@ -115,12 +115,12 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0a001e;
-        public static final int notification_action_tombstone = 0x7f0a001f;
-        public static final int notification_template_custom_big = 0x7f0a0020;
-        public static final int notification_template_icon_group = 0x7f0a0021;
-        public static final int notification_template_part_chronometer = 0x7f0a0022;
-        public static final int notification_template_part_time = 0x7f0a0023;
+        public static final int notification_action = 0x7f0a0020;
+        public static final int notification_action_tombstone = 0x7f0a0021;
+        public static final int notification_template_custom_big = 0x7f0a0022;
+        public static final int notification_template_icon_group = 0x7f0a0023;
+        public static final int notification_template_part_chronometer = 0x7f0a0024;
+        public static final int notification_template_part_time = 0x7f0a0025;
     }
     public static final class string {
         private string() {}
---
+++ SportGymApp/app/src/main/java/com/example/sportgymapp/DefinicoesActivity.java
@@ -0,0 +1,14 @@
+package com.example.sportgymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DefinicoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_definicoes);
+    }
+}
